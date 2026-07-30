58308eda9e26b3747afb06f892611539b83a02f3
ashmanofficial <ashley.thompson@lavere.co.uk>
2026-07-30 13:16:22 +0100

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import LavereWebsite from './LavereWebsite';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <LavereWebsite />
+  </React.StrictMode>
+);
